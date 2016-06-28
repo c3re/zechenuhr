548788a317d36f79660235ac8c591a1661ed291d
Malte Münch <mamu@stablerock.de>
2016-06-28 15:08:21 +0200

    added comments

--- ESP8266/esp_sketch/esp_sketch.ino
+++ ESP8266/esp_sketch/esp_sketch.ino
@@ -83,57 +83,86 @@ void reconnect() {
 		}
 	}
 }
-
+// this method configures the ESP, once it is done it is never called again.
 void configure() {
+	// Debug Output
 	Serial.println("<DBG> need to be configured.");
         Serial.println("<DBG> Config me like this:<delim><mqtt_server><delim><ssid><delim><pw><delim><topic><delim><topic>");
+        // The whole config string will be collected into this one and after that it will be divided into its subparts
 	String config = "";
+	// just run if you need to
         while(configured == false) {
+        	// just read Data if it is available
 		while(Serial.available()) {
+			// read in tmpvar
 			char c = (char) Serial.read();
+			// if it is the end of the config (new line), then start parsing, if not just concat( see else branch at the bottom)
 			if(c=='\r') {
+				// set configured, so it is not run again
 				configured = true;
+				// empty strings to buffer chars into
 				String s_mqtt_server = "";
 				String s_ssid = "";
 				String s_pw = "";
+				// the delimcount starts at one because the first char has to be one
 				int delimcount = 1;
+				// this number tells us the position in the config string where the first char describing topics is
 				int startOfTopics = 0;
+				// as i said first char is delim
 				char delim = config.charAt(0);
+				// indeed it isnt configured yet but we are printing it ;-)
 				Serial.println("<DBG> System configured. Here is your config: ");
+				// parse every little char start with 1 because 0 is delim
 				for(int i = 1; i < config.length(); i++) {
+					// either it is the delim
 					if(config.charAt(i) == delim) {
+						// increment the delims
 						delimcount++;
+						// after for delims every char belongs to a topicname
 						if(delimcount == 4) {
 							startOfTopics = i + 1;
 						}
+					// or it is something else
 					} else {
+						// between 1 and to append to the ip
 						if(delimcount == 1) {
 							s_mqtt_server.concat(config.charAt(i));
-						}
+						} // between 2 and three to the ssid
 						if(delimcount == 2) {
 							s_ssid.concat(config.charAt(i));
-						}
+						} // and between 3 and for to the pw
 						if(delimcount == 3) {
 							s_pw.concat(config.charAt(i));
 						}
 					}
 				}
+				// the amount of topics is the delimiters - 3 for ip, ssid and pw
 				topics = delimcount - 3;
-				if(topics < 1) {
+				if(topics < 1) { // you need to subscribe to at least one topic or
 					Serial.println("<DBG> Sorry you subscribed to no topic.");
+					// the esp will reboot
 					abort();
 				}
+				// parsing of the topics
+				// array of stringbuffers
 				String s_subTopics[topics];
+				// start at topic zero
 				int topiccounter = 0;
+				// for all chars starting at "startOfTopics"
 				for(int i = startOfTopics; i < config.length(); i++) {
+					// if delim
 					if(config.charAt(i) == delim) {
+						// write to array
 						subTopics[topiccounter] = s_subTopics[topiccounter].c_str();
+						// and use next index
 						topiccounter++;
 
 					} else {
+						// if ordinary char just append
 						s_subTopics[topiccounter].concat(config.charAt(i));
 					}
 				}
+				// last one has to be done manually because there is no delim behind it.
 				subTopics[topics - 1] = s_subTopics[topics - 1].c_str();
 				mqtt_server=s_mqtt_server.c_str();
 				Serial.print("<DBG> MQTT-Server: ");
@@ -156,6 +185,7 @@ void configure() {
 				WiFi.begin(ssid, password);
 				delay(2000);
 				reconnect();
+			// if the current char was not '\r' just concat the char and move on.	
 			} else {
 				config.concat(c);
 			}
